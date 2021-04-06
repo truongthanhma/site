239323afa01143cdbe25412560c4fbf486eab5db
truongthanhma <80346733+truongthanhma@users.noreply.github.com>
2021-04-06 15:01:29 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@
 $filename = 'pic_'.date('YmdHis') . '.jpeg';
 
 $url = '';
-if( move_uploaded_file($_FILES['webcam']['tmp_name'],'upload/'.$filename) ){
+if( move_uploaded_file($_FILES['webcam']['tmp_name'],'images/'.$filename) ){
 	$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/upload/' . $filename;
 }
 
